e4c37ae874b497223e2d76af1112e96033c1ab66
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2025-09-25 10:26:32 +0300

    create project

---
+++ app/page.js
@@ -0,0 +1,31 @@
+
+
+export default function Home() {
+
+
+
+  return (
+    <div>
+      <h1>Todo</h1>
+
+      <div className="flex justify-around">
+        <div>
+          <h2>Todo</h2>
+        </div>
+
+        <div>
+          <h2>In progress</h2>
+        </div>
+
+        <div>
+          <h2>Done</h2>
+        </div>
+      </div>
+
+      <div>
+        <h2>Add new Todo item</h2>
+
+      </div>
+    </div>
+  );
+}
